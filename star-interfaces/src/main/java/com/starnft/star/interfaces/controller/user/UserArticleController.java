0e55b51c5f3fdbfa3b351acbb33a8d96e11e92eb
Harlan <harlan.liu@profitgrp.com>
2022-06-09 13:30:30 +0800

    feature: 获取用户藏品相关接口优化

--- star-interfaces/src/main/java/com/starnft/star/interfaces/controller/user/UserArticleController.java
+++ star-interfaces/src/main/java/com/starnft/star/interfaces/controller/user/UserArticleController.java
@@ -9,6 +9,8 @@ import com.starnft.star.domain.article.model.vo.UserNumbersVO;
 import com.starnft.star.domain.article.model.vo.UserSeriesVO;
 import com.starnft.star.domain.article.model.vo.UserThemeVO;
 import com.starnft.star.domain.article.service.UserThemeService;
+import com.starnft.star.interfaces.interceptor.UserResolverInfo;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,27 +18,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/article")
+@Api(tags = "用户收藏相关接口「UserArticleController」")
 public class UserArticleController {
     @Resource
     private UserThemeService userThemeService;
 
-    @ApiOperation("个人系列")
+    @ApiOperation("获取用户收藏的系列")
     @PostMapping("/series")
-    public RopResponse<ResponsePageResult<UserSeriesVO>> series(@RequestBody UserHaveSeriesReq req) {
-        return RopResponse.success(userThemeService.queryUserArticleSeriesInfo(req));
+    public RopResponse<ResponsePageResult<UserSeriesVO>> series(UserResolverInfo userResolverInfo, @RequestBody UserHaveSeriesReq req) {
+        req.setUserId(userResolverInfo.getUserId());
+        return RopResponse.success(this.userThemeService.queryUserArticleSeriesInfo(req));
     }
 
-    @ApiOperation("个人系列主题")
-    @PostMapping("/theme")
-    public RopResponse<ResponsePageResult<UserThemeVO>> theme(@RequestBody UserHaveThemeReq req) {
-        return RopResponse.success(userThemeService.queryUserArticleThemeInfo(req));
+    @ApiOperation("获取用户收藏的主题")
+    @PostMapping("/themes")
+    public RopResponse<ResponsePageResult<UserThemeVO>> theme(UserResolverInfo userResolverInfo, @RequestBody UserHaveThemeReq req) {
+        req.setUserId(userResolverInfo.getUserId());
+        return RopResponse.success(this.userThemeService.queryUserArticleThemeInfo(req));
     }
 
-    @ApiOperation("个人系列主题数字")
-    @PostMapping("/number")
-    public RopResponse<ResponsePageResult<UserNumbersVO>> numbers(@RequestBody UserHaveNumbersReq req) {
-        return RopResponse.success(userThemeService.queryUserArticleNumberInfo(req));
+    @ApiOperation("获取用户收藏的藏品")
+    @PostMapping("/numbers")
+    public RopResponse<ResponsePageResult<UserNumbersVO>> numbers(UserResolverInfo userResolverInfo, @RequestBody UserHaveNumbersReq req) {
+        req.setUserId(userResolverInfo.getUserId());
+        return RopResponse.success(this.userThemeService.queryUserArticleNumberInfo(req));
     }
 }
