1dcf4a6aa5a3eef61bc64638a99fc2e5f046b748
Harlan <harlan.liu@profitgrp.com>
2022-06-14 21:25:57 +0800

    feature: 我的藏品添加发行商信息

--- star-domain/src/main/java/com/starnft/star/domain/article/model/vo/UserThemeVO.java
+++ star-domain/src/main/java/com/starnft/star/domain/article/model/vo/UserThemeVO.java
@@ -1,13 +1,24 @@
 package com.starnft.star.domain.article.model.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel
 public class UserThemeVO implements Serializable {
+    @ApiModelProperty("主题id")
     private Long themeId;
+    @ApiModelProperty("主题名称")
     private String themeName;
+    @ApiModelProperty("主题图片")
     private String themeImages;
+    @ApiModelProperty("主题藏品数量")
     private Integer nums;
+    @ApiModelProperty("发行商名称")
+    private String publisherName;
+    @ApiModelProperty("发行商图标")
+    private String publisherPic;
 }
