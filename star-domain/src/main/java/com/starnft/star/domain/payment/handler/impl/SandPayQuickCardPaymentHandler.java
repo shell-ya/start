43114f04a9dd04d79625ee2e77446c9e1ac1f439
huangkunhuang <jacquesh@88.com>
2022-06-15 14:00:21 +0800

    快捷绑卡

--- star-domain/src/main/java/com/starnft/star/domain/payment/handler/impl/SandPayQuickCardPaymentHandler.java
+++ star-domain/src/main/java/com/starnft/star/domain/payment/handler/impl/SandPayQuickCardPaymentHandler.java
@@ -3,6 +3,8 @@ package com.starnft.star.domain.payment.handler.impl;
 import cn.hutool.json.JSONUtil;
 import com.google.common.collect.Maps;
 import com.starnft.star.common.constant.StarConstants;
+import com.starnft.star.common.exception.StarException;
+import com.starnft.star.common.utils.Assert;
 import com.starnft.star.domain.payment.helper.SdKeysHelper;
 import com.starnft.star.domain.payment.helper.TemplateHelper;
 import com.starnft.star.domain.payment.model.req.PaymentRich;
@@ -30,8 +32,8 @@ public class SandPayQuickCardPaymentHandler extends AbstractSandPayHandler {
 
     @Override
     protected void verifyLegality(PaymentRich req) {
-//        Assert.isNull(req.getFrontUrl(), () -> new StarException("回显地址不能为空"));
-//        Assert.isNull(req.getUserId(), () -> new StarException("用户id不能为空"));
+        Assert.isNull(req.getFrontUrl(), () -> new StarException("回显地址不能为空"));
+        Assert.isNull(req.getUserId(), () -> new StarException("用户id不能为空"));
 //       Assert.isNull(req.getTotalMoney(),()->new StarException("用户id不能为空"));
     }
 
