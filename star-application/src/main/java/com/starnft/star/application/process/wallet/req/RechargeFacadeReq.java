57fb65bbeb7a58ef92edd132f212eea45ce8d5a0
huangkunhuang <jacquesh@88.com>
2022-06-18 13:52:10 +0800

    用户是否设置密码接口

--- star-application/src/main/java/com/starnft/star/application/process/wallet/req/RechargeFacadeReq.java
+++ star-application/src/main/java/com/starnft/star/application/process/wallet/req/RechargeFacadeReq.java
@@ -35,4 +35,6 @@ public class RechargeFacadeReq implements Serializable {
     @NotBlank(message = "渠道不能为空")
     @ApiModelProperty(value = "渠道 AliPay 支付宝/UNION_PAY 云闪付/WeChatPay 微信/BankCard 银行卡/Balance 余额", required = true)
     private String channel;
+
+
 }
