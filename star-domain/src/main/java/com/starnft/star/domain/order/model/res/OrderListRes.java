0acd57911c622f7508adfa73ce579c5418abc011
张泽 <18332204521@163.com>
2022-06-26 23:35:07 +0800

    溢出

--- star-domain/src/main/java/com/starnft/star/domain/order/model/res/OrderListRes.java
+++ star-domain/src/main/java/com/starnft/star/domain/order/model/res/OrderListRes.java
@@ -1,6 +1,8 @@
 package com.starnft.star.domain.order.model.res;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -14,6 +16,7 @@ import java.util.Date;
 public class OrderListRes implements Serializable {
 
     @ApiModelProperty(value = "订单id")
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
     @ApiModelProperty(value = "订单号")
     private String orderSn;
