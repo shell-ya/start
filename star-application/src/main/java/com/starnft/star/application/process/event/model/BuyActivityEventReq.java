4d323fb685d2acbe0492aa95557e4e358aaa7504
huangkunhuang <jacquesh@88.com>
2022-07-06 17:52:45 +0800

    排行版

--- star-application/src/main/java/com/starnft/star/application/process/event/model/BuyActivityEventReq.java
+++ star-application/src/main/java/com/starnft/star/application/process/event/model/BuyActivityEventReq.java
@@ -2,10 +2,11 @@ package com.starnft.star.application.process.event.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
-public class BuyActivityEventReq extends  BaseActivityEventReq{
+public class BuyActivityEventReq  extends  BaseActivityEventReq implements Serializable{
     private Long seriesId;
     //购买的主题id
     private Long themeId;
--- star-application/src/main/java/com/starnft/star/application/process/rank/strategy/action/RankBuyActionState.java
+++ star-application/src/main/java/com/starnft/star/application/process/rank/strategy/action/RankBuyActionState.java
@@ -1,10 +1,15 @@
 package com.starnft.star.application.process.rank.strategy.action;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.json.JSONUtil;
 import com.starnft.star.application.process.event.model.ActivityEventReq;
+import com.starnft.star.application.process.event.model.BuyActivityEventReq;
 import com.starnft.star.common.constant.StarConstants;
 import com.starnft.star.domain.rank.core.rank.model.RankDefinition;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -16,5 +21,9 @@ public class RankBuyActionState implements  IRankActionState {
     @Override
     public void manage(ActivityEventReq activityEventReq, RankDefinition rankDefinition) {
         log.info("进入拉新排行版购买动作");
+        Map<String, Object> params = activityEventReq.getParams();
+        BuyActivityEventReq buyActivityEventReq = BeanUtil.mapToBean(params, BuyActivityEventReq.class, true);
+       log.info("进入参数为「{}」", JSONUtil.toJsonStr(buyActivityEventReq));
+
     }
 }
