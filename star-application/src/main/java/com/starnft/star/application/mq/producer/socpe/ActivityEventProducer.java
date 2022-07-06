d76d8c425e1799b61b1c9f87564147f810e594a3
RyannZ <isleo9608@gmail.com>
2022-07-06 12:19:09 +0800

    优化

--- star-application/src/main/java/com/starnft/star/application/mq/producer/socpe/ActivityEventProducer.java
+++ star-application/src/main/java/com/starnft/star/application/mq/producer/socpe/ActivityEventProducer.java
@@ -1,10 +1,8 @@
 package com.starnft.star.application.mq.producer.socpe;
 
-import cn.hutool.json.JSONUtil;
 import com.starnft.star.application.mq.constant.TopicConstants;
 import com.starnft.star.application.mq.producer.BaseProducer;
 import com.starnft.star.application.process.event.model.ActivityEventReq;
-import lombok.Data;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,27 +13,4 @@ public class ActivityEventProducer extends BaseProducer {
         messageSender.send(topic, Optional.of(activityEventReq));
     }
 
-
-
-
-
-    public static void main(String[] args) {
-//        @Data
-//        class Params{
-//            private String template;
-//            private BigDecimal scale;
-//            private Integer scopeType;
-//        }
-        @Data
-        class Params{
-            private String rankName;
-//            private BigDecimal scale;
-//            private Integer scopeType;
-        }
-        Params params = new Params();
-        params.setRankName("test_tank");
-//        params.setTemplate("参加拉新活动获得%s积分");
-//        params.setScopeType(0);
-        System.out.println(JSONUtil.toJsonStr(params));
-    }
 }
