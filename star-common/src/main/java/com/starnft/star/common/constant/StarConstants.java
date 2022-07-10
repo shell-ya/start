3e9398816bc4cf2a740d407ce3edc413e9bcc8f8
张泽 <18332204521@163.com>
2022-07-10 17:41:52 +0800

    merge缺失enum

--- star-common/src/main/java/com/starnft/star/common/constant/StarConstants.java
+++ star-common/src/main/java/com/starnft/star/common/constant/StarConstants.java
@@ -422,4 +422,59 @@ public class StarConstants {
         }
     }
 
+    public enum themeResaleEnum{
+        NOT_RESALE(0,"不可转售"),
+        RESALE(1,"可转售"),
+        ;
+        private Integer code;
+        private String desc;
+        themeResaleEnum(int code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+        public Integer getCode() {
+            return code;
+        }
+        public void setCode(Integer code) {
+            this.code = code;
+        }
+        public String getDesc() {
+            return desc;
+        }
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+    }
+
+
+    public enum PropsType {
+
+        CONSUMPTION(1, "消耗品"),
+        PERMANENT(2, "长期存在的"),
+        ;
+        private Integer code;
+        private String desc;
+
+        PropsType(Integer code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public void setCode(Integer code) {
+            this.code = code;
+        }
+    }
+
 }
