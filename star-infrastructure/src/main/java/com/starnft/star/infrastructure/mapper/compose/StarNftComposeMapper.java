315ab358b358dfd0b637664b81c006d83777b539
huangkunhuang <jacquesh@88.com>
2022-07-25 21:31:21 +0800

    Revert "合成"
    This reverts commit 3bb2716c22b11f6195b962dabf351701679ec8dd.

--- star-infrastructure/src/main/java/com/starnft/star/infrastructure/mapper/compose/StarNftComposeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.starnft.star.infrastructure.mapper.compose;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.starnft.star.infrastructure.entity.compose.StarNftCompose;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface StarNftComposeMapper extends BaseMapper<StarNftCompose> {
-}
