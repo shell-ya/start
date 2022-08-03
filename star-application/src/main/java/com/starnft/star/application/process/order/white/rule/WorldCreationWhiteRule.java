b5870eb92f9ae9c5b6f8456e1d7b10eb07e68d19
RyannZ <isleo9608@gmail.com>
2022-08-03 14:50:53 +0800

    首发

--- star-application/src/main/java/com/starnft/star/application/process/order/white/rule/WorldCreationWhiteRule.java
+++ star-application/src/main/java/com/starnft/star/application/process/order/white/rule/WorldCreationWhiteRule.java
@@ -2,14 +2,19 @@ package com.starnft.star.application.process.order.white.rule;
 
 import com.starnft.star.common.constant.RedisKey;
 import com.starnft.star.domain.component.RedisUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 @Service("worldCreationWhiteRule")
 public class WorldCreationWhiteRule extends AbstractWhiteRule {
+
+    private static final Logger log = LoggerFactory.getLogger(WorldCreationWhiteRule.class);
     @Resource
     RedisUtil redisUtil;
+
 
     @Override
     public boolean verifyRule(Long uid, Long themeId) {
@@ -18,7 +23,7 @@ public class WorldCreationWhiteRule extends AbstractWhiteRule {
             redisUtil.hincr(RedisKey.SECKILL_GOODS_PRIORITY_TIMES.getKey(), String.valueOf(uid), 1L);
             times = redisUtil.hdecr(RedisKey.SECKILL_GOODS_PRIORITY_TIMES.getKey(), String.valueOf(uid), 1L);
         }
-
+        log.info("uid :[{}] 当前优先购次数：[{}]", uid, times);
         if (times > 0L) {
             return true;
         }
