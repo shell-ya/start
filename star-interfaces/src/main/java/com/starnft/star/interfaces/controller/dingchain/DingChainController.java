35e2d2cc96f8accb77af53f1f3e4e8f8b535c188
huangkunhuang <jacquesh@88.com>
2022-08-19 17:25:36 +0800

    盯链

--- star-interfaces/src/main/java/com/starnft/star/interfaces/controller/dingchain/DingChainController.java
+++ star-interfaces/src/main/java/com/starnft/star/interfaces/controller/dingchain/DingChainController.java
@@ -1,18 +1,22 @@
 package com.starnft.star.interfaces.controller.dingchain;
 
+import com.google.common.collect.Maps;
 import com.starnft.star.domain.number.model.vo.NumberDingVO;
 import com.starnft.star.domain.number.serivce.INumberService;
 import com.starnft.star.interfaces.interceptor.TokenIgnore;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.compress.utils.Lists;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Api(tags = "图像验证码相关接口「CaptchaController」")
@@ -31,8 +35,14 @@ public class DingChainController {
 
     @RequestMapping("bulletin")
     @TokenIgnore
-    public List<NumberDingVO> bulletin(){
-        return iNumberService.getNumberDingList();
+    public List<Map<String,Object>> bulletin(){
+        Map<@Nullable String, @Nullable Object> resultMap = Maps.newHashMap();
+        resultMap.put("title","40万实物邀新");
+        resultMap.put("image","https://7463-tcb-jsq5g2gn216911-8dtv73c1de35f-1312710945.tcb.qcloud.la/img/76771660875671_.pic.jpg");
+        resultMap.put("link","https://mp.weixin.qq.com/s/Zw1rxIi5z9DjieYZUUw7OA");
+        ArrayList<Map<@Nullable String, @Nullable Object>> result = Lists.newArrayList();
+        result.add(resultMap);
+        return result;
     }
 
 //    @RequestMapping("price")
