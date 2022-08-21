cdea8f0f5f2854bc5b8c38322eda46e9ebadbcd9
huangkunhuang <jacquesh@88.com>
2022-08-21 22:47:43 +0800

    转赠

--- star-domain/src/main/java/com/starnft/star/domain/given/model/req/GivenMangeReq.java
+++ star-domain/src/main/java/com/starnft/star/domain/given/model/req/GivenMangeReq.java
@@ -1,12 +1,21 @@
 package com.starnft.star.domain.given.model.req;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+
 @Data
+@ApiModel
 public class GivenMangeReq {
+    @ApiModelProperty("手机号")
     private String mobile;
+    @ApiModelProperty("支付密码")
     private String payPassword;
+    @ApiModelProperty("编码ID")
     private Long numberId;
+    @ApiModelProperty("编码ID")
     private Long themeId;
+    @ApiModelProperty("编码ID")
     private Long seriesId;
 }
