139c17627182e215e107e2cf0c7bb196000e7064
laiwc <2681737638@qq.com>
2022-09-09 21:03:44 +0800

    序列化问题

--- star-common/src/main/java/com/starnft/star/common/page/ResponsePageResult.java
+++ star-common/src/main/java/com/starnft/star/common/page/ResponsePageResult.java
@@ -5,13 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResponsePageResult<T> {
+public class ResponsePageResult<T> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private List<T> list;
     private Integer page;
     private Integer size;
