40985ec5e01b5f4160437a79358f393ae32267f6
zz <1208084818@qq.com>
2022-10-09 20:49:32 +0800

    涨停数据表

---
+++ star-infrastructure/src/main/java/com/starnft/star/infrastructure/entity/raising/StarRaisingDetail.java
@@ -0,0 +1,9 @@
+package com.starnft.star.infrastructure.entity.raising;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("star_ra")
+public class StarRaisingDetail {
+}
