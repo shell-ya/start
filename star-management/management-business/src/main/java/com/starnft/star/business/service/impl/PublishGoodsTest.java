10cbbe106589723eaee357f3720db8576dab93a8
shenfan <shenfan@yiling.cn>
2022-10-27 10:41:18 +0800

    发布商品,优化逻辑，并行请求_shenfan

--- star-management/management-business/src/main/java/com/starnft/star/business/service/impl/PublishGoodsTest.java
+++ star-management/management-business/src/main/java/com/starnft/star/business/service/impl/PublishGoodsTest.java
@@ -1,6 +1,7 @@
 package com.starnft.star.business.service.impl;
 
 import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.SecureUtil;
@@ -9,14 +10,14 @@ import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.google.common.collect.Lists;
 import com.starnft.star.common.chain.model.req.PublishGoodsReq;
-import com.starnft.star.common.chain.model.res.PublishGoodsRes;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.commons.collections4.ListUtils;
 
-import java.util.Date;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class PublishGoodsTest {
-
 
     /**
      * 测试调用天河链接口数字商品发行
@@ -24,47 +25,78 @@ public class PublishGoodsTest {
      * @param args
      */
     public static void main(String[] args) {
-
-
-
-        int publishNumber = 2000;
-        int pageSize = 600;
-
-        // 请求次数
-        // int requestTimes = (publishNumber % pageSize) == 0 ? (publishNumber / pageSize) : (publishNumber / pageSize + 1);
-        String today = cn.hutool.core.date.DateUtil.format(new Date(), DatePattern.PURE_DATE_PATTERN);
-        String prefix = today + RandomUtil.randomString("0", String.valueOf(publishNumber).length());
-        Long baseId = Long.valueOf(prefix);
-        List<Long> container = Lists.newArrayListWithCapacity(publishNumber);
-        for (int i = 0; i < publishNumber; i++) {
-            long valueId = baseId + i;
-            container.add(valueId);
-        }
-
-        List<List<Long>> subs = ListUtils.partition(container, pageSize);
-        System.out.println(subs);
-
-        // PublishGoodsRes.DataDTO result = new PublishGoodsRes.DataDTO();
-        // result.getProducts().add(new PublishGoodsRes.DataDTO.ProductsDTO());
-        // System.out.println(result);
-
-        //
-        // PublishGoodsReq publishGoodsReq = new PublishGoodsReq();
-        // publishGoodsReq.setUserId("951029971223");
-        // String userKey = SecureUtil.sha1("951029971223".concat("lywc"));
-        // publishGoodsReq.setUserKey(userKey);
-        // publishGoodsReq.setAuthor("链元文创");
-        // publishGoodsReq.setInitPrice("1");
-        // publishGoodsReq.setName(String.format("链元文创 %s-%s", "test", "主题"));
-        //
-        // publishGoodsReq.setProductIds(productIds);
-        // publishGoodsReq.setPieceCount(publishNumber);
-        //
-        // JSONObject json = JSONUtil.parseObj(publishGoodsReq);
-        // json.set("appId", "tichain449113");
-        // json.set("appKey", "a070713d25c291127992bf096cbcb3462ca1e33c");
-        //
-        // String result = HttpUtil.post("https://api.tichain.tianhecloud.com/api/v2/nfr/publish", JSONUtil.toJsonStr(json));
-        // System.out.println(result);
+        int publishNumber = 5;
+        int pageSize = 2;
+        String baseString = "0";
+        String today = cn.hutool.core.date.DateUtil.format(DateUtil.date(), DatePattern.PURE_DATE_PATTERN);
+        String prefix = today + RandomUtil.randomString(baseString, String.valueOf(publishNumber).length());
+        List<String> container = Lists.newArrayListWithCapacity(publishNumber);
+        IntStream.range(0, publishNumber).forEach(item -> container.add(String.valueOf(Long.parseLong(prefix) + item)));
+        List<List<String>> subs = ListUtils.partition(container, pageSize);
+        subs.parallelStream().forEach(item -> {
+            PublishRequest request = new PublishRequest();
+            request.setName("test");
+            request.setProductIds(item);
+            request.setInitPrice("1");
+            request.setPieceCount(item.size());
+            String result = HttpUtil.post("https://api.tichain.tianhecloud.com/api/v2/nfr/publish", JSONUtil.toJsonStr(request));
+            System.out.println(result);
+        });
     }
+
+}
+
+@NoArgsConstructor
+@Data
+class PublishRequest {
+
+    /**
+     * appId
+     */
+    private String appId = "tichain449113";
+
+    /**
+     * appKey
+     */
+    private String appKey = "a070713d25c291127992bf096cbcb3462ca1e33c";
+
+    /**
+     * userId
+     */
+    private String userId = "951029971223";
+
+    /**
+     * userKey
+     */
+    private String userKey = SecureUtil.sha1("951029971223".concat("lywc"));
+
+    /**
+     * productIds
+     */
+    private List<String> productIds;
+
+    /**
+     * name
+     */
+    private String name;
+
+    /**
+     * pieceCount
+     */
+    private Integer pieceCount;
+
+    /**
+     * author
+     */
+    private String author = "链元文创";
+
+    /**
+     * feature
+     */
+    private String feature;
+
+    /**
+     * 发行价格，字符长度不超过 20
+     */
+    private String initPrice;
 }
