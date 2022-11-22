a465ac10efacb29c8d1789015943e0bf8144d9a6
shenfan <shenfan@yiling.cn>
2022-11-22 15:51:52 +0800

    修改订单超时时间为5m_shenfan

--- star-application/src/main/java/com/starnft/star/application/mq/producer/order/OrderProducer.java
+++ star-application/src/main/java/com/starnft/star/application/mq/producer/order/OrderProducer.java
@@ -47,7 +47,7 @@ public class OrderProducer extends BaseProducer {
 
         String destination = String.format(TopicConstants.MARKET_ORDER_ROLLBACK_DESTINATION.getFormat(), TopicConstants.MARKET_ORDER_ROLLBACK_DESTINATION.getTag());
 
-        messageSender.syncSendDelay(destination, Optional.of(status), 3000L, 7);
+        messageSender.syncSendDelay(destination, Optional.of(status), 3000L, 9);
     }
 
     private Consumer<SendResult> success() {
